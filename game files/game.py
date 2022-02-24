2f82449a4315d8415c0c02480a35de18ecfdd03e
GuilhermeSilva <99082595+GuilhermeSDSilva@users.noreply.github.com>
2022-02-24 12:01:38 -0300

    Update game.py

--- game files/game.py
+++ game files/game.py
@@ -41,3 +41,29 @@ def game_loop_multi():
     while config.multi:
         Screen_multi(screen_dimensions["width"], screen_dimensions["height"])
         pygame.display.update()
+ 
+
+class Score():
+    pontos_1 = 0
+    pontos_2 = 0
+    
+    def player_1(self):
+        self.pontos_1 +=1 
+        return self.pontos_1
+    
+    def player_2(self):
+        self.pontos_2 +=1
+        return self.pontos_2
+        
+
+    def placar(self):
+        score_font = pygame.font.Font('PressStart2P.ttf', 30)
+        score_text = score_font.render('00 x 00', True, "white", "black")
+        score_text_rect = score_text.get_rect()
+        score_text_rect.center = (480, 30)
+        score_text = score_font.render(str(self.pontos_1) + ' x ' + str(self.pontos_2), True, "white", "black")
+        screen.blit(score_text, score_text_rect)
+    
+
+    
+
