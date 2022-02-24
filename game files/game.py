163bca5327d894e2735cad4494653cfcc793b851
Fernando <ffdc.snf21@uea.edu.br>
2022-02-23 23:05:44 -0300

    Funções para seleção de Single ou Multi Player

--- game files/game.py
+++ game files/game.py
@@ -1,15 +1,31 @@
 import pygame
-from screen import Screen
-from config import screen_dimensions
+
 import config
+from config import screen_dimensions
+from screen import Screen
+
 
 def comands_verifying():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             config.playing = False
 
+
+pygame.init()
+pygame.mixer.init()
+
+test_sound1 = pygame.mixer.Sound("assets/brick.wav")
+test_sound2 = pygame.mixer.Sound("assets/paddle.wav")
+
+
 def game_loop():
     while config.playing:
 
-        Screen(screen_dimensions['width'], screen_dimensions['height'])
+        Screen(screen_dimensions["width"], screen_dimensions["height"])
         pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            elif event.type == pygame.K_m:
+                test_sound1.play()
