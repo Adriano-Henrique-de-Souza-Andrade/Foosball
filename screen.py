b14d3332b51a444c90628f623d651e8cb90a99eb
Monike Freitas <monike.ftsousa@gmail.com>
2022-02-27 18:08:21 -0400

    ♻ screen code review fix

--- screen.py
+++ screen.py
@@ -1,4 +1,3 @@
-from audioop import mul
 import pygame
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_WIDTH, TRANSITION_TIME, COLUMN_COLORS, PIPE_WIDTH
 from colors import Colors
@@ -15,7 +14,6 @@ def draw(caption, player_coords, ball_coord, pipe_blue, pipe_red, column_kicking
     global count
     global pause_time
     self = Screen(caption)
-    pygame.display.set_caption(caption)
     floor = pygame.image.load("img/floor.png").convert_alpha()
     table = pygame.image.load("img/table.png").convert_alpha()
     border = pygame.image.load("img/border.png").convert_alpha()
@@ -106,12 +104,153 @@ def draw(caption, player_coords, ball_coord, pipe_blue, pipe_red, column_kicking
 
 class Screen:
     surface: pygame.Surface
+    player_coords = []
+    blue_pipe = 0
+    red_pipe = 0
+    column_kicking = -1
+    count = 0
+
+    pause_start = -100
+    pause_end = -100
+
+    result_start = -100
+    
+    ball_coord = (0, 0)
+    score = (0, 0)
 
     def __init__(self, title):
         self.width = SCREEN_WIDTH
         self.height = SCREEN_HEIGHT
         self.surface = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
         pygame.display.set_caption(title)
+
+        self.floor = pygame.image.load("img/floor.png").convert_alpha()
+        self.table = pygame.image.load("img/table.png").convert_alpha()
+        self.border = pygame.image.load("img/border.png").convert_alpha()
+        self.placar = pygame.image.load("img/placar.png").convert_alpha()
+        self.ball = pygame.image.load("img/ball.png").convert_alpha()
+        self.pipe_down = pygame.image.load("img/pipe_down.png").convert_alpha()
+        self.pipe_up = pygame.image.load("img/pipe_up.png").convert_alpha()
+        self.player_red = [
+            pygame.image.load("img/player_red.png").convert_alpha(),
+            pygame.image.load("img/player_red_kicking_left.png").convert_alpha(),
+            pygame.image.load("img/player_red_kicking_right.png").convert_alpha(),
+        ]
+
+        self.player_blue = [
+            pygame.image.load("img/player_blue.png").convert_alpha(),
+            pygame.image.load("img/player_blue_kicking_left.png").convert_alpha(),
+            pygame.image.load("img/player_blue_kicking_right.png").convert_alpha(),
+        ]
+        self.board = pygame.image.load("img/board.png").convert_alpha()
+
+
+    def set_pipes(self, blue, red):
+        self.blue_pipe = blue
+        self.red_pipe = red
+
+    def set_players(self, player_coords):
+        self.player_coords = player_coords
+
+    def set_score(self, score):
+        self.score = score
+
+    def set_ball(self, ball_coord):
+        self.ball_coord = ball_coord
+
+    def set_pause(self, pause):
+        if pause:
+            if self.pause_start < 0:
+                self.pause_start = self.count
+            self.pause_end = -100
+        else:
+            if self.pause_end < 0:
+                self.pause_end = self.count
+            self.pause_start = -100
+
+    def draw_pipes(self, col_index):
+        column = self.player_coords[col_index]
+
+        if COLUMN_COLORS[col_index] == "red":
+            self.surface.blit(
+                self.pipe_up, (column[0][0] - PIPE_WIDTH / 2, self.red_pipe))
+        else:
+            self.surface.blit(
+                self.pipe_down, (column[0][0] - PIPE_WIDTH / 2, self.blue_pipe))
+
+    def draw_players_column(self, col_index):
+        column_state = 0
+        column = self.player_coords[col_index]
+
+        if self.column_kicking == col_index:
+            if column[0][0] > self.ball_coord[0]:
+                column_state = 1
+            else:
+                column_state = 2
+
+        for position in column:
+            if COLUMN_COLORS[col_index] == "red":
+                player_position = (
+                    position[0] - PLAYER_WIDTH / 2, position[1])
+                self.surface.blit(
+                    self.player_red[column_state], player_position)
+            else:
+                player_position = (
+                    position[0] - PLAYER_WIDTH / 2, position[1])
+                self.surface.blit(
+                    self.player_blue[column_state], player_position)
+
+    def draw(self):
+        transition = 3
+        animation_count = -1
+
+        self.surface.blit(self.floor, (0, 0))
+        self.surface.blit(self.table, (75, 60))
+        self.surface.blit(self.ball, self.ball_coord)
+
+        for i in range(len(self.player_coords)):
+            self.draw_pipes(i)
+            self.draw_players_column(i)
+
+        self.surface.blit(self.border, (75, 60))
+        self.surface.blit(self.placar, (0, 0))
+
+        direction = 0
+        pause_time = 0
+
+        if self.pause_start > 0:
+            direction = 0
+            pause_time = self.pause_start
+        else:
+            direction = transition
+            pause_time = self.pause_end
+        
+        if self.count - pause_time < transition:
+            if direction == 0:
+                animation_count = (self.count - pause_time)  
+            else:
+                animation_count = direction - (self.count - pause_time) 
+        elif self.count == transition:
+            animation_count = transition+0.2 - direction
+        else:
+            animation_count = transition - direction
+
+        if animation_count > 0 or animation_count > transition:
+            behind = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+            behind.set_alpha(animation_count*(128/transition))
+            behind.fill(Colors["Black"])
+
+            self.surface.blit(behind, (0, 0))
+            self.surface.blit(self.board, (0, animation_counter(-100, animation_count, self.pause_start)))
+
+            pygame.draw.rect(self.surface,
+                                Colors["White"], [460, 240 + animation_counter(-100, animation_count, transition), 15, 50])
+            pygame.draw.rect(self.surface,
+                                Colors["White"], [485, 240 + animation_counter(-100, animation_count, transition), 15, 50])
+
+        self.count += 1
+        print(self.count, animation_count)
+        pygame.display.update()
 
 
 class ScreenSelection:
