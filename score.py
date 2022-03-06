718c254ff7ce793e9cfd065e5adacd52f6e9e153
Fernando <ffdc.snf21@uea.edu.br>
2022-03-06 17:03:53 -0300

    Removendo lógica do game over

--- score.py
+++ score.py
@@ -21,20 +21,3 @@ class Score():
         score_text_rect.center = (480, 30)
         score_text = score_font.render(str(self.pontos_1) + ' x ' + str(self.pontos_2), True, "white", "black")
         screen.blit(score_text, score_text_rect)
-
-        if self.pontos_1 == 7:
-            font = pygame.font.Font("fonts/Pixeled.ttf", 70)
-            text = font.render("GAME OVER - Player 1 Wins", 1, (255,255,255))
-            text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 1.5))
-            screen.blit(text, text_rect)
-            pygame.display.update()
-            pygame.time.wait(2000)
-            run = False
-        if self.pontos_2 == 7:
-            font = pygame.font.Font("fonts/Pixeled.ttf", 70)
-            text = font.render("GAME OVER - Player 2 Wins", 1, (255,255,255))
-            text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 1.5))
-            screen.blit(text, text_rect)
-            pygame.display.update()
-            pygame.time.wait(2000)
-            run = False
